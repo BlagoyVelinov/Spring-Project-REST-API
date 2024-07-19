8837adec4d2108e51770483206b36ef285a7c8ef
Blagoy <blagoy1234@gmail.com>
2024-07-19 22:34:32 +0300

    Create REST for movies.

---
+++ src/main/java/bg/softuni/CinemaTickets_Movies/CinemaTicketsMoviesApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.CinemaTickets_Movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketsMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/CinemaTickets_Movies/CinemaTicketsMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.CinemaTickets_Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketsMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
